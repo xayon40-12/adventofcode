38ebaba0a2dbe0aaf22e6fb6e3f8603839f83cf4
Touroux Nathan <touroux.nathan@gmail.com>
2017-12-15 15:21:11 +0100

    add second part of day14

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,7 +1,10 @@
 package Day14
 
+import com.sun.org.apache.xpath.internal.operations.Bool
+
 fun main(args: Array<String>) {
-    assert(compute("flqrgnkx") == 8108)//check the example of the subject
+    println("compute(\"flqrgnkx\")=${compute("flqrgnkx")} == 8108")//check the example of the subject
+    println("countRegions(\"flqrgnkx\")=${countRegions("flqrgnkx")} == 1242")//check the example of the subject
 
     val input = input()
 
@@ -32,22 +35,46 @@ fun compute(s: String): Int{
 }
 
 fun countRegions(s: String): Int{
-    var grid = ""
+    var grid = Array(128, {IntArray(128)})
     //generate a grid with empty an full squares
-    for(i in 0..127){//for each lines
-        val knotHash = Day10.dense(Day10.parse(Day10.prepareToAscii("$s-$i"), 64))//compute the knot hash
-        for(c in knotHash.toCharArray()){//for each digit of the line find the 0 and 1 bits and add a " " or "#" corresponding
+    for(y in 0..127){//for each lines
+        var x = 0
+        val knotHash = Day10.dense(Day10.parse(Day10.prepareToAscii("$s-$y"), 64))//compute the knot hash
+        for(c in knotHash.toCharArray()){//for each digit of the line find the 0 and 1 bits and put it in grid
             val digit = Integer.parseInt(c.toString(), 16)
-            for(i in 0..3){
-                if (digit and (1 shl i) > 0) grid += "#"
-                else grid += " "
+            for(i in 3 downTo 0){//from 3 to 0 because it needs to go from left to right
+                if (digit and (1 shl i) > 0) grid[y][x] = 1
+                else grid[y][x] = 0
+                x++
             }
         }
-        if(i != 127) grid += "\n"
     }
     var count = 0
-    //TODO count how many group there is 
+
+    //for each square equal to 1 in the grid remove the block attached and increment count if a block was removed
+    for(y in 0..127){
+        for(x in 0..127){
+            if(removeBolck(grid, x, y)) count++
+        }
+    }
+
     return count
+}
+
+fun removeBolck(grid: Array<IntArray>, x: Int , y: Int, minx: Int = 0, miny: Int = 0, maxx: Int = 127, maxy: Int = 127): Boolean{
+    if((minx<=x) and (x<=maxx) and (miny<=y) and (y<=maxy)){//if the coordinate is inside the square
+        if(grid[x][y] == 1) {//if the square is full
+            grid[x][y] = 0//empty the square
+            removeBolck(grid, x + 1, y)//then do the same for each contiguous squares
+            removeBolck(grid, x, y + 1)
+            removeBolck(grid, x - 1, y)
+            removeBolck(grid, x, y - 1)
+
+            return true//and return true because a block was removed
+        }
+    }
+
+    return false//if both condition weren't true no block was removed so return false
 }
 
 fun input(): String{
